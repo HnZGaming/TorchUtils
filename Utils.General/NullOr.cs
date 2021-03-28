d19f2e945dc914d3113aca05f55c0f4e76ecc09f
Ryoichiro Oka <ryoka0418@gmail.com>
2021-03-29 05:34:35 +0900

    nullor

---
+++ Utils.General/NullOr.cs
@@ -0,0 +1,35 @@
+﻿namespace Utils.General
+{
+    public sealed class NullOr<T> where T : class
+    {
+        readonly T _instance;
+
+        public NullOr()
+        {
+        }
+
+        public NullOr(T instance)
+        {
+            _instance = instance;
+        }
+
+        public bool TryGet(out T instance)
+        {
+            instance = _instance;
+            return instance != null;
+        }
+    }
+
+    public static class NullOr
+    {
+        public static NullOr<T> NotNull<T>(T instance) where T : class
+        {
+            return new NullOr<T>(instance);
+        }
+
+        public static NullOr<T> Null<T>() where T : class
+        {
+            return new NullOr<T>();
+        }
+    }
+}
