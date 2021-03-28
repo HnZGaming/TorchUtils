9612fd00944533829aa6876f90f66e06ba9a70e5
Ryoichiro Oka <ryoka0418@gmail.com>
2021-03-28 21:06:59 +0900

    dont implicitly move to different thread

--- Utils.Torch/CommandModuleUtils.cs
+++ Utils.Torch/CommandModuleUtils.cs
@@ -15,11 +15,10 @@ namespace Utils.Torch
 {
     public static class CommandModuleUtils
     {
-        public static async void CatchAndReport(this CommandModule self, Action f)
+        public static void CatchAndReport(this CommandModule self, Action f)
         {
             try
             {
-                await TaskUtils.MoveToThreadPool();
                 f();
             }
             catch (Exception e)
@@ -32,7 +31,6 @@ namespace Utils.Torch
         {
             try
             {
-                await TaskUtils.MoveToThreadPool();
                 await f();
             }
             catch (Exception e)
