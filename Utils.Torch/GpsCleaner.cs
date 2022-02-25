e35f5a4a2ba0e492acc96ef82a965c32cd1897f5
Ryoichiro Oka <ryoka0418@gmail.com>
2022-02-25 12:27:37 +0900

    gps cleaner

---
+++ Utils.Torch/GpsCleaner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Sandbox.Game;
+using VRageMath;
+
+namespace Utils.Torch
+{
+    public sealed class GpsCleaner
+    {
+        readonly long _playerId;
+        readonly HashSet<string> _names;
+
+        public GpsCleaner(long playerId)
+        {
+            _playerId = playerId;
+            _names = new HashSet<string>();
+        }
+
+        public void Add(string name, string description, Vector3D position, Color color)
+        {
+            MyVisualScriptLogicProvider.AddGPS(name, description, position, color, playerId: _playerId);
+            _names.Add(name);
+        }
+
+        public void Clear()
+        {
+            foreach (var name in _names)
+            {
+                MyVisualScriptLogicProvider.RemoveGPS(name, _playerId);
+            }
+
+            _names.Clear();
+        }
+    }
+}
---
+++ Utils.Torch/GpsCleanerCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using VRageMath;
+
+namespace Utils.Torch
+{
+    public sealed class GpsCleanerCollection
+    {
+        readonly Dictionary<long, GpsCleaner> _gpsCleaners;
+
+        public GpsCleanerCollection()
+        {
+            _gpsCleaners = new Dictionary<long, GpsCleaner>();
+        }
+
+        public void Add(long playerId, string name, string description, Vector3D position, Color color)
+        {
+            if (!_gpsCleaners.TryGetValue(playerId, out var gpsCleaner))
+            {
+                _gpsCleaners[playerId] = gpsCleaner = new GpsCleaner(playerId);
+            }
+
+            gpsCleaner.Add(name, description, position, color);
+        }
+
+        public void Clear(long playerId)
+        {
+            if (_gpsCleaners.TryGetValue(playerId, out var gpsCleaner))
+            {
+                gpsCleaner.Clear();
+                _gpsCleaners.Remove(playerId);
+            }
+        }
+    }
+}
