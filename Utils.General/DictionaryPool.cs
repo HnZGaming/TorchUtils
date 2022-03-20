ded534c17bcad406b40f7ae7b9197c9ca80f6bb6
Ryoichiro Oka <ryoka0418@gmail.com>
2022-03-20 21:32:39 +0900

    dictionary pool

---
+++ Utils.General/DictionaryPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Utils.General
+{
+    public static class DictionaryPool<K, V>
+    {
+        static readonly ConcurrentBag<Dictionary<K, V>> _pool = new();
+
+        public static Dictionary<K, V> Create()
+        {
+            if (_pool.TryTake(out var e)) return e;
+            return new Dictionary<K, V>();
+        }
+
+        public static void Release(Dictionary<K, V> e)
+        {
+            e.Clear();
+            _pool.Add(e);
+        }
+    }
+}
