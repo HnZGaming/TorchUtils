18d5b65d472018ca75c0c9ad0e1256253733a215
Ryoichiro Oka <ryoka0418@gmail.com>
2022-03-20 12:31:41 +0900

    no thread check

--- Utils.TorchEntityGps/PrefixedGpsCollection.cs
+++ Utils.TorchEntityGps/PrefixedGpsCollection.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using Sandbox.Game.Multiplayer;
 using Sandbox.Game.Screens.Helpers;
 using Sandbox.Game.World;
-using Utils.Torch;
 using Utils.Torch.Patches;
 using VRage.Game.ModAPI;
 
@@ -37,8 +35,6 @@ namespace Utils.TorchEntityGps
 
         public IEnumerable<(long IdentityId, MyGps Gps)> GetAllGpss()
         {
-            Thread.CurrentThread.ThrowIfNotSessionThread();
-
             foreach (var (identityId, gps) in Native.GetAllGpss())
             {
                 if (gps == null) continue;
@@ -52,8 +48,6 @@ namespace Utils.TorchEntityGps
 
         public IEnumerable<MyGps> GetPlayerGpss(long identityId)
         {
-            Thread.CurrentThread.ThrowIfNotSessionThread();
-
             var gpss = new List<IMyGps>();
             Native.GetGpsList(identityId, gpss);
             return gpss.Cast<MyGps>().Where(g => IsOurs(g));
@@ -61,8 +55,6 @@ namespace Utils.TorchEntityGps
 
         public void SendDeleteAllGpss()
         {
-            Thread.CurrentThread.ThrowIfNotSessionThread();
-
             foreach (var (identityId, gps) in GetAllGpss().ToArray())
             {
                 Native.SendDelete(identityId, gps.Hash);
@@ -71,22 +63,12 @@ namespace Utils.TorchEntityGps
 
         public void SendAddGps(long identityId, MyGps gps, bool playSound)
         {
-            Thread.CurrentThread.ThrowIfNotSessionThread();
-
             MarkOurs(gps);
             Native.SendAddGps(identityId, ref gps, gps.EntityId, playSound);
         }
 
         public void SendDeleteGps(long identityId, int gpsHash)
         {
-            Thread.CurrentThread.ThrowIfNotSessionThread();
-
-            var gps = Native.GetGps(gpsHash);
-            if (!IsOurs(gps))
-            {
-                throw new Exception($"not ours: {gps.DisplayName}");
-            }
-
             Native.SendDelete(identityId, gpsHash);
         }
     }
