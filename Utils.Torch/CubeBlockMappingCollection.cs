913476b7f5c36af21f8398043c568fe0b66aa5df
Ryoichiro Oka <ryoka0418@gmail.com>
2022-04-27 15:07:54 +0900

    protobuf

--- Utils.Torch/CubeBlockMappingCollection.cs
+++ Utils.Torch/CubeBlockMappingCollection.cs
@@ -8,17 +8,14 @@ namespace DefaultNamespace
 {
     public sealed class CubeBlockMappingCollection<E, T> where E : IMyCubeBlock
     {
-        public interface IMapper
-        {
-            T Map(E entity);
-        }
+        public delegate T Map(E entity);
 
         readonly ISceneEntityAddRemoveObserver<E> _observer;
-        readonly IMapper _mapper;
+        readonly Map _mapper;
         readonly CachingDictionary<long, T> _mappedEntities;
         readonly Throttle<T> _throttle;
 
-        public CubeBlockMappingCollection(ISceneEntityAddRemoveObserver<E> observer, IMapper mapper)
+        public CubeBlockMappingCollection(ISceneEntityAddRemoveObserver<E> observer, Map mapper)
         {
             _observer = observer;
             _mapper = mapper;
@@ -46,7 +43,7 @@ namespace DefaultNamespace
 
         void OnEntityAdded(E block)
         {
-            var mappedBlock = _mapper.Map(block);
+            var mappedBlock = _mapper(block);
             lock (_mappedEntities)
             {
                 _mappedEntities.Add(block.EntityId, mappedBlock);
