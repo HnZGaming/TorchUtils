4c0116911d3200b3aec8219a4475b9f4fada6b54
Ryoichiro Oka <ryoka0418@gmail.com>
2025-03-13 00:16:58 +0900

    feat: ObservableSet

---
+++ Utils.Torch/ObservableSet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Utils.Torch
+{
+    internal sealed class ObservableSet<T>
+    {
+        HashSet<T> _self = new();
+        ObservableCollection<T> _source;
+
+        public void SetSource(ObservableCollection<T> source)
+        {
+            _self = new HashSet<T>(source);
+
+            if (_source != null)
+            {
+                _source.CollectionChanged -= OnSourceChanged;
+            }
+
+            _source = source;
+            _source.CollectionChanged += OnSourceChanged;
+        }
+
+        void OnSourceChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add when e.NewItems != null:
+                {
+                    foreach (T item in e.NewItems)
+                    {
+                        _self.Add(item);
+                    }
+
+                    break;
+                }
+                case NotifyCollectionChangedAction.Remove when e.OldItems != null:
+                {
+                    foreach (T item in e.OldItems)
+                    {
+                        _self.Remove(item);
+                    }
+
+                    break;
+                }
+                case NotifyCollectionChangedAction.Reset:
+                {
+                    _self = new HashSet<T>(_source);
+                    break;
+                }
+                case NotifyCollectionChangedAction.Replace:
+                case NotifyCollectionChangedAction.Move:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            return _self.Contains(item);
+        }
+    }
+}
